9c7ed45caaadd150eac1aeede30d5964cdc11f2f
覃国强 <qinguoqiang@xiaomi.com>
2020-09-04 08:40:20 +0800

    Feature: git auto push

--- src/main/java/com/leetcode/程序员面试宝典/面试题_08_02_迷路的机器人.java
+++ src/main/java/com/leetcode/程序员面试宝典/面试题_08_02_迷路的机器人.java
@@ -1,9 +1,8 @@
 package com.leetcode.程序员面试宝典;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -12,26 +11,80 @@ import java.util.Objects;
  */
 public class 面试题_08_02_迷路的机器人 {
 
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int[][] obstacleGrid = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+            {0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0},
+            {1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1},
+            {1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1},
+            {0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1},
+            {1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0}
+        };
+
+        List<List<Integer>> lists = solution.pathWithObstacles(obstacleGrid);
+        System.out.println(lists);
+        System.out.println("==============");
+
+        int[][] test = solution.testPathWithObstacles(obstacleGrid);
+
+        for (int i = 0; i < test.length; ++i) {
+            for (int j = 0; j < test[i].length; ++j) {
+                System.out.print(test[i][j]);
+            }
+            System.out.println();
+        }
+
+
+        System.out.println("==============");
+
+        int[][] obstacleGrid2 = {
+            {0}
+        };
+        lists = solution.pathWithObstacles(obstacleGrid2);
+        System.out.println(lists);
+        System.out.println("==============");
+
+    }
+
 
     static class Solution {
 
-        public List<List<Integer>> pathWithObstacles(int[][] obstacleGrid) {
+        public int[][] testPathWithObstacles(int[][] obstacleGrid) {
 
-            Map<Coordinate, List<Coordinate>> pathMap = new HashMap<>();
 
-            List<Coordinate> path = new ArrayList<>();
-            path.add(new Coordinate(0, 0));
-            pathMap.put(new Coordinate(0, 0), path);
+            int[][] tmp = new int[obstacleGrid.length][obstacleGrid[0].length];
+
+            if (obstacleGrid[0][0] == 1) {
+                return tmp;
+            }
+
+            tmp[0][0] = 1;
 
             // 处理第一行
             for (int j = 1; j < obstacleGrid[0].length; ++j) {
                 if (obstacleGrid[0][j] == 1) {
                     break;
                 }
-                List<Coordinate> prePath = pathMap.get(new Coordinate(0, j - 1));
-                List<Coordinate> curPath = new ArrayList<>(prePath);
-                curPath.add(new Coordinate(0, j));
-                pathMap.put(new Coordinate(0, j), curPath);
+                tmp[0][j] = 1;
             }
 
             // 处理第一列
@@ -39,46 +92,100 @@ public class 面试题_08_02_迷路的机器人 {
                 if (obstacleGrid[i][0] == 1) {
                     break;
                 }
-                List<Coordinate> prePath = pathMap.get(new Coordinate(i - 1, 0));
-                List<Coordinate> curPath = new ArrayList<>(prePath);
-                curPath.add(new Coordinate(i, 0));
-                pathMap.put(new Coordinate(i, 0), curPath);
+                tmp[i][0] = 1;
+            }
+
+
+            for (int i = 1; i < obstacleGrid.length; ++i) {
+                for (int j = 1; j < obstacleGrid[i].length; ++j) {
+                    if (obstacleGrid[i][j] == 1) {
+                        continue;
+                    }
+
+                    if (tmp[i - 1][j] != 1 && tmp[i][j - 1] != 1) {
+                        continue;
+                    }
+
+                    tmp[i][j] = 1;
+
+                }
+            }
+
+            return tmp;
+
+        }
+
+        public List<List<Integer>> pathWithObstacles(int[][] obstacleGrid) {
+            if (obstacleGrid[0][0] == 1) {
+                return new ArrayList<>();
+            }
+
+            if (obstacleGrid.length == 1 && obstacleGrid[0].length == 1) {
+                List<List<Integer>> result = new ArrayList<>();
+                List<Integer> list = new ArrayList<>();
+                list.add(0);
+                list.add(0);
+                result.add(list);
+                return result;
+            }
+
+            Coordinate[][] preCoors = new Coordinate[obstacleGrid.length][obstacleGrid[0].length];
+
+            // 处理第一行
+            for (int j = 1; j < obstacleGrid[0].length; ++j) {
+                if (obstacleGrid[0][j] == 1) {
+                    break;
+                }
+                preCoors[0][j] = new Coordinate(0, j - 1);
+            }
+
+            // 处理第一列
+            for (int i = 1; i < obstacleGrid.length; ++i) {
+                if (obstacleGrid[i][0] == 1) {
+                    break;
+                }
+                preCoors[i][0] = new Coordinate(i - 1, 0);
             }
 
             for (int i = 1; i < obstacleGrid.length; ++i) {
                 for (int j = 1; j < obstacleGrid[i].length; ++j) {
-                    if (obstacleGrid[i][j] == 1 ||
-                        (obstacleGrid[i][j - 1] == 1 && obstacleGrid[i - 1][j] == 1)) {
+
+                    if (obstacleGrid[i][j] == 1) {
                         continue;
                     }
-                    List<Coordinate> prePath;
-                    if (obstacleGrid[i][j - 1] != 1) {
-                        prePath = pathMap.get(new Coordinate(i, j - 1));
-                    } else {
-                        prePath = pathMap.get(new Coordinate(i - 1, j));
+
+                    if (preCoors[i - 1][j] == null && preCoors[i][j - 1] == null) {
+                        continue;
                     }
-                    List<Coordinate> curPath = new ArrayList<>(prePath);
-                    curPath.add(new Coordinate(i, j));
-                    pathMap.put(new Coordinate(i, j), curPath);
+
+                    if (preCoors[i][j - 1] != null) {
+                        preCoors[i][j] = new Coordinate(i, j - 1);
+                    } else {
+                        preCoors[i][j] = new Coordinate(i - 1, j);
+                    }
+
                 }
             }
 
-            List<Coordinate> coordinates = pathMap
-                .get(new Coordinate(obstacleGrid.length - 1, obstacleGrid[0].length - 1));
-
-            if (coordinates == null || coordinates.isEmpty()) {
+            if (preCoors[obstacleGrid.length - 1][obstacleGrid[0].length - 1] == null) {
                 return new ArrayList<>();
             }
 
-            List<List<Integer>> res = new ArrayList<>();
-            for (Coordinate coordinate : coordinates) {
-                List<Integer> list = new ArrayList<>();
-                list.add(coordinate.x);
-                list.add(coordinate.y);
-                res.add(list);
-            }
+            LinkedList<List<Integer>> result = new LinkedList<>();
+            List<Integer> last = new ArrayList<>();
+            last.add(obstacleGrid.length - 1);
+            last.add(obstacleGrid[0].length - 1);
+            result.add(last);
 
-            return res;
+            Coordinate curCoor = preCoors[obstacleGrid.length - 1][obstacleGrid[0].length - 1];
+            while (curCoor != null) {
+                List<Integer> tmp = new ArrayList<>();
+                tmp.add(curCoor.x);
+                tmp.add(curCoor.y);
+                result.addFirst(tmp);
+                curCoor = preCoors[curCoor.x][curCoor.y];
+            }
+            return result;
         }
     }
 
---
+++ src/main/java/com/leetcode/程序员面试宝典/面试题_08_04_幂集.java
@@ -0,0 +1,82 @@
+package com.leetcode.程序员面试宝典;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * @author 覃国强
+ * @date 2020-09-04 08:20
+ */
+public class 面试题_08_04_幂集 {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int[] nums = {1, 2, 3};
+
+        List<List<Integer>> subsets = solution.subsets(nums);
+
+        System.out.println(subsets);
+
+    }
+
+   static class Solution {
+        public List<List<Integer>> subsets(int[] nums) {
+
+            if (nums == null || nums.length == 0) {
+                return new ArrayList<>();
+            }
+
+            if (nums.length == 1) {
+                List<List<Integer>> result = new ArrayList<>();
+                result.add(new ArrayList<>());
+                List<Integer> list = new ArrayList<>();
+                list.add(nums[0]);
+                result.add(list);
+                return result;
+            }
+
+            Deque<List<Integer>> cur = new ArrayDeque<>();
+            Deque<List<Integer>> next = new ArrayDeque<>();
+
+            List<List<Integer>> indexes = new ArrayList<>();
+
+            for (int i = 0; i < nums.length; ++i) {
+                List<Integer> list = new ArrayList<>();
+                list.add(i);
+                cur.add(list);
+            }
+
+            while (!cur.isEmpty()) {
+                while (!cur.isEmpty()) {
+                    List<Integer> poll = cur.poll();
+                    indexes.add(new ArrayList<>(poll));
+
+                    for (int i = poll.get(poll.size() - 1) + 1; i < nums.length; ++i) {
+                        List<Integer> list = new ArrayList<>(poll);
+                        list.add(i);
+                        next.add(list);
+                    }
+                }
+                cur = next;
+                next = new ArrayDeque<>();
+            }
+
+            List<List<Integer>> result = new ArrayList<>();
+            result.add(new ArrayList<>());
+
+            for (List<Integer> index : indexes) {
+                List<Integer> numList = new ArrayList<>();
+                for (int i : index) {
+                    numList.add(nums[i]);
+                }
+                result.add(numList);
+            }
+            return result;
+        }
+    }
+
+}
