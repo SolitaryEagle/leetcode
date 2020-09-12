d29e979c01aa0f8d9d60bdd29799ab6cbee37fc1
覃国强 <qinguoqiang@xiaomi.com>
2020-09-12 11:27:32 +0800

    Feature: git auto push

--- src/main/java/com/leetcode/程序员面试宝典/面试题_08_14_布尔运算.java
+++ src/main/java/com/leetcode/程序员面试宝典/面试题_08_14_布尔运算.java
@@ -6,6 +6,15 @@ package com.leetcode.程序员面试宝典;
  */
 public class 面试题_08_14_布尔运算 {
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String s = "1^0|0|1";
+
+        System.out.println(solution.countEval(s, 0));
+
+    }
+
     static class Solution {
 
         public int countEval(String s, int result) {
@@ -23,27 +32,29 @@ public class 面试题_08_14_布尔运算 {
                 dp[i][i][1] = chars[i] == '1' ? 1 : 0;
             }
 
-            for (int i = 0; i < chars.length; i += 2) {
-                for (int j = i + 2; j < chars.length; j += 2) {
-                    for (int k = i + 1; k <= j; k += 2) {
+            // 已知对角线的值，需从对角线向上移动求值
+
+            for (int j = 2; j < chars.length; j += 2) {
+                for (int i = j - 2; i >= 0; i -= 2) {
+                    for (int k = i + 1; k < j; k += 2) {
                         if (chars[k] == '&') {
-                            dp[i][j][0] = dp[i][k - 1][0] * dp[k + 1][j][0];
-                            dp[i][j][0] = dp[i][k - 1][0] * dp[k + 1][j][1];
-                            dp[i][j][0] = dp[i][k - 1][1] * dp[k + 1][j][0];
+                            dp[i][j][0] += dp[i][k - 1][0] * dp[k + 1][j][0];
+                            dp[i][j][0] += dp[i][k - 1][0] * dp[k + 1][j][1];
+                            dp[i][j][0] += dp[i][k - 1][1] * dp[k + 1][j][0];
 
-                            dp[i][j][1] = dp[i][k - 1][1] * dp[k + 1][j][1];
+                            dp[i][j][1] += dp[i][k - 1][1] * dp[k + 1][j][1];
                         } else if (chars[k] == '|') {
-                            dp[i][j][0] = dp[i][k - 1][0] * dp[k + 1][j][0];
+                            dp[i][j][0] += dp[i][k - 1][0] * dp[k + 1][j][0];
 
-                            dp[i][j][1] = dp[i][k - 1][0] * dp[k + 1][j][1];
-                            dp[i][j][1] = dp[i][k - 1][1] * dp[k + 1][j][0];
-                            dp[i][j][1] = dp[i][k - 1][1] * dp[k + 1][j][1];
+                            dp[i][j][1] += dp[i][k - 1][0] * dp[k + 1][j][1];
+                            dp[i][j][1] += dp[i][k - 1][1] * dp[k + 1][j][0];
+                            dp[i][j][1] += dp[i][k - 1][1] * dp[k + 1][j][1];
                         } else {
-                            dp[i][j][0] = dp[i][k - 1][0] * dp[k + 1][j][0];
-                            dp[i][j][0] = dp[i][k - 1][1] * dp[k + 1][j][1];
+                            dp[i][j][0] += dp[i][k - 1][0] * dp[k + 1][j][0];
+                            dp[i][j][0] += dp[i][k - 1][1] * dp[k + 1][j][1];
 
-                            dp[i][j][1] = dp[i][k - 1][0] * dp[k + 1][j][1];
-                            dp[i][j][1] = dp[i][k - 1][1] * dp[k + 1][j][0];
+                            dp[i][j][1] += dp[i][k - 1][0] * dp[k + 1][j][1];
+                            dp[i][j][1] += dp[i][k - 1][1] * dp[k + 1][j][0];
                         }
                     }
                 }
