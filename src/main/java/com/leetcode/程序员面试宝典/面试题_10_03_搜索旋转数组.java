c701875621c78b6603e839ecc3e5dd7460a64f59
覃国强 <qinguoqiang@xiaomi.com>
2020-09-15 14:28:31 +0800

    Feature: git auto push

--- src/main/java/com/leetcode/程序员面试宝典/面试题_10_03_搜索旋转数组.java
+++ src/main/java/com/leetcode/程序员面试宝典/面试题_10_03_搜索旋转数组.java
@@ -10,10 +10,10 @@ public class 面试题_10_03_搜索旋转数组 {
 
         Solution solution = new Solution();
 
-        int[] arr = {15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14};
 
-        System.out.println(solution.findMinIndex(arr));
+        int[] arr = {10, 12, 13, 15, 15, 15, 17, 19, 24, 26, 30, 30, 33, 35, 35, 36, 37, 39, 39, 41, 45, 48, 49, 49, 50, 52, 54, 55, 57, 62, 66, 70, 73, 85, 85, 87, 89, 93, 96, 96, 99, 99, -98, -91, -88, -87, -84, -82, -81, -81, -79, -78, -77, -75, -73, -69, -67, -64, -64, -61, -58, -57, -55, -55, -52, -49, -48, -40, -36, -35, -31, -22, -20, -20, -20, -20, -18, -15, -14, -14, -13, -5, -5, -4, -4, -3, -2, -1, 0, 2, 2, 3, 4, 5, 6, 7, 8, 8, 9, 9};
 
+        System.out.println(solution.search(arr, 165310062));
     }
 
 
@@ -25,65 +25,70 @@ public class 面试题_10_03_搜索旋转数组 {
                 return -1;
             }
 
-            int minIndex = findMinIndex(arr);
-
-            return 0;
-        }
-
-        private int search(int[] arr, int begin, int end, int target) {
-
-            while (begin <= end) {
-                int mid = (begin + end) / 2;
-                if (arr[mid] == target) {
-                    if (mid > 0 && arr[mid - 1] == arr[mid]) {
-                        end = mid - 1;
-                    } else {
-                        return mid;
-                    }
-                } else if (arr[mid] < arr[begin]) {
-                    if (arr[mid] < target) {
-
-                    }
-                }
-            }
-
-            return 0;
-        }
-
-        private int findMinIndex(int[] arr) {
             if (arr.length == 1) {
-                return 0;
+                return arr[0] == target ? 0 : -1;
             }
 
-            if (arr[0] < arr[arr.length - 1]) {
-                return 0;
-            }
+            return specialBinarySearch(arr, target);
+        }
 
-            if (arr[0] > arr[1]) {
-                return 1;
-            }
-
-            if (arr[arr.length - 2] > arr[arr.length - 1]) {
-                return arr.length - 1;
-            }
-
-            int begin = 1;
-            int end = arr.length - 2;
+        private int normalBinarySearch(int[] arr, int begin, int end, int target) {
 
             while (begin <= end) {
-                int mid = (begin + end) / 2;
-                if (arr[mid - 1] > arr[mid] && arr[mid] < arr[mid + 1]) {
-                    return mid;
+                if (arr[begin] == target) {
+                    return begin;
                 }
-                if (arr[mid] < arr[end]) {
-                    end = mid - 1;
-                } else if (arr[mid] > arr[end]) {
+                int mid = (begin + end) / 2;
+                if (arr[mid] < target) {
                     begin = mid + 1;
+                } else if (arr[mid] > target) {
+                    end = mid - 1;
+                } else {
+                    end = mid;
                 }
             }
 
-            return 0;
+            return -1;
         }
+
+        private int specialBinarySearch(int[] arr, int target) {
+
+            int begin = 0;
+            int end = arr.length - 1;
+
+            while (begin <= end) {
+                if (arr[begin] < arr[end]) {
+                    return normalBinarySearch(arr, begin, end, target);
+                }
+
+                if (arr[begin] == target) {
+                    return begin;
+                }
+                if (begin == end) {
+                    return -1;
+                }
+                int mid = (begin + end) / 2;
+                if (arr[begin] < arr[mid]) {
+                    if (arr[begin] < target && target <= arr[mid]) {
+                        end = mid;
+                    } else {
+                        begin = mid;
+                    }
+                } else if (arr[begin] > arr[mid]) {
+                    if (arr[begin] < target || target <= arr[mid]) {
+                        end = mid;
+                    } else {
+                        begin = mid;
+                    }
+                } else {
+                    // 这里应该如何优化？
+                    begin++;
+                }
+            }
+
+            return -1;
+        }
+
     }
 
 }
